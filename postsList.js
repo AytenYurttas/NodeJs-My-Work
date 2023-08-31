52e33133765f7154c8efd8355d2a1401c4397446
AytenYurttas <aytenyurttas1107@gmail.com>
2023-08-31 15:43:48 +0300

    postsList.js
    patika odev 2

---
+++ postsList.js
@@ -0,0 +1,49 @@
+const myPosts = [
+    {
+      id: "1",
+      title: "Javascript",
+      body: "Lorem ipsum dolar...!",
+      date: "2023-03-24",
+    },
+    {
+      id: "2",
+      title: "Typescript",
+      body: "Lorem ipsum dolar...!",
+      date: "2023-03-24",
+    },
+    {
+      id: "3",
+      title: "TailwinsUI",
+      body: "Lorem ipsum dolar...!",
+      date: "2023-03-24",
+    },
+    {
+      id: "4",
+      title: "Material UI",
+      body: "Lorem ipsum dolar...!",
+      date: "2023-03-24",
+    },
+  ];
+
+  const listThePost =()=>{
+    myPosts.map((post)=>console.log(post));
+  };
+
+  const addTheNewPost = () =>{
+    myPosts.push({
+        id:"5",
+        title:"Next.JS",
+        body: "Lorem ipsum dolar...!",
+        date: "2023-03-25",
+    });
+  };
+
+  const showThePost = async () =>{
+    try{
+        await addTheNewPost();
+        listThePost();
+    }catch(error){
+        console.log(error);
+    }
+  };
+  showThePost();
